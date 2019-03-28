97663668b6085573be894ca8736d9a59e1a58261
Terry Tan <tmy1995hflc@gmail.com>
2019-03-28 14:10:06 -0500

    change io

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
-const express = require("express");
-const app = express();
-var server = require('http').Server(app);
-var io = require('socket.io')(server);
+const app = require("express")();
+var server = require('http').createServer(app);
+var io = require('socket.io').listen(server);
 
 let connectedUsers = [];
 let userCards = new Map();
